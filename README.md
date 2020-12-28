b47c6cdea525d02f135b064275fde8c457e160c9
Jet Chien <67193495+jet-chien@users.noreply.github.com>
2020-12-28 21:51:40 +0800

    update read me, pymysql get data

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Horus
 A Human Tracking System
+
+## Dev Environment
+> database - MySQL 8.0
+
+> anaconda - python 3.7.9
+```bash
+conda create --name horus python=3.7 -y
+```
--- TEST/db_test_2.py
+++ TEST/db_test_2.py
@@ -1,7 +1,8 @@
+import pandas as pd
 import pymysql
 
 
-def get_db_conn():
+def get_db_conn() -> pymysql.connections.Connection:
     host = '163.14.137.58'
     port = 8080
     user = 'admin'
@@ -18,7 +19,7 @@ def get_db_conn():
         print(f'error: {e}')
 
 
-def create_table(conn):
+def create_table(conn: pymysql.connections.Connection):
     with conn.cursor() as cursor:
         query = """
         CREATE TABLE IF NOT EXISTS `teacher`( \
@@ -30,7 +31,8 @@ def create_table(conn):
         """
         cursor.execute(query)
 
-def insert_data(conn):
+
+def insert_data(conn: pymysql.connections.Connection):
     with conn.cursor() as cursor:
         query = """
         INSERT INTO `test`.`teacher` 
@@ -41,10 +43,35 @@ def insert_data(conn):
         cursor.execute(query)
 
 
-conn = get_db_conn()
-# create_table(conn)
-insert_data(conn)
+def get_data(conn: pymysql.connections.Connection) -> list:
+    with conn.cursor() as cursor:
+        query = """
+        SELECT * FROM test.teacher
+        """
+        cursor.execute(query)
+        return cursor.fetchall()
 
-conn.commit()
-conn.close()
-print('done')
+
+def get_data_as_df(conn: pymysql.connections.Connection) -> pd.DataFrame:
+    # return pd.read_sql_table('teacher', conn) # only supported for SQLAlchemy connectable.
+    query = """
+    SELECT * FROM test.teacher
+    """
+    # return pd.read_sql(query, conn) # ok
+    return pd.read_sql_query(query, conn)  # ok
+
+
+conn = get_db_conn()
+
+df = get_data_as_df(conn)
+print(df)
+
+data = get_data(conn)
+print(data)
+
+# # create_table(conn)
+# insert_data(conn)
+#
+# conn.commit()
+# conn.close()
+# print('done')
