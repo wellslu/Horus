d90381e765303bf408d8b28b7b68aeee9cf5087a
Jet Chien <67193495+jet-chien@users.noreply.github.com>
2021-01-08 15:20:11 +0800

    update submod

--- test_import.py
+++ test_import.py
@@ -4,4 +4,4 @@ GitHub: https://github.com/jet-chien
 Create Date: 2021/1/8
 """
 # coding: utf-8
-from FaceRecog.Facer.shortcut import get_face_grid_from_portrait
+from .FaceRecog import test_import
