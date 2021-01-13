8eadfa3564b1b93fed48479a2ff9f378b9c31a1b
Jet Chien <67193495+jet-chien@users.noreply.github.com>
2021-01-13 17:23:09 +0800

    update horus toolkit ult

--- FaceRecog/horus_toolkit/__init__.py
+++ FaceRecog/horus_toolkit/__init__.py
@@ -8,6 +8,8 @@ Create Date: 2021/1/13
 # function
 from .tk_gen import gen_random_token
 from .ult import gen_member_id, sec_to_hms
+from .ult import get_face_recog_helper
 
 # class
 from .timer import UpdateTimer
+from .face_recog_helper import FaceRecogHelper
--- FaceRecog/horus_toolkit/ult.py
+++ FaceRecog/horus_toolkit/ult.py
@@ -7,6 +7,9 @@ Create Date: 2021/1/13
 from typing import Union
 import time
 import datetime
+from pymysql.connections import Connection
+from .face_recog_helper import FaceRecogHelper
+from ..Facer import FaceCapturer, LMKScanner, AGFaceRecog
 
 from .tk_gen import gen_random_token
 
@@ -28,3 +31,17 @@ def sec_to_hms(sec: Union[float, int]) -> str:
     if isinstance(sec, float):
         sec = int(sec)
     return str(datetime.timedelta(seconds=sec))
+
+
+def get_face_recog_helper(face_capturer: FaceCapturer, lmk_scanner: LMKScanner,
+                          ag_face_recog: AGFaceRecog, mf_data: dict,
+                          fr_db_conn: Connection, member_table_name: str, customer_table_name: str) -> FaceRecogHelper:
+    return FaceRecogHelper(
+        face_capturer,
+        lmk_scanner,
+        ag_face_recog,
+        mf_data,
+        fr_db_conn,
+        member_table_name,
+        customer_table_name
+    )
