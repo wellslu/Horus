c21226740d15f1a0a1be075b6843290213fc7734
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2021-01-15 01:55:31 +0800

    debug

--- JDE/utils/visualization.py
+++ JDE/utils/visualization.py
@@ -93,7 +93,7 @@ def plot_tracking(image, cid_png, tlwhs, obj_ids, sql, opt, scores=None, frame_i
         color = get_color(abs(int(str(obj_id)[-1])))
         if mid is None:
             cv2.rectangle(im, intbox[0:2], intbox[2:4], color=color, thickness=line_thickness)
-            cv2.putText(im, id_text, (intbox[0], intbox[1] + 30), cv2.FONT_HERSHEY_PLAIN,
+            cv2.putText(im, str(id_text), (intbox[0], intbox[1] + 30), cv2.FONT_HERSHEY_PLAIN,
                         text_scale, (0, 0, 255), thickness=text_thickness)
         else:
             cv2.rectangle(im, intbox[0:2], intbox[2:4], (208, 216, 129), thickness=line_thickness)
