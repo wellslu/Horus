92d7d939fe320587039b607a8afa7091186252c4
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2021-01-15 03:36:04 +0800

    Update run_videos.sh

--- run_videos.sh
+++ run_videos.sh
@@ -6,6 +6,15 @@ for v in $video;
 do
   input=input_video/"$v"
   output=output_video/"$v"
-  python main.py --input-video "$input" --output "$output" --output-root "$video"
+  python main.py --input-video "$input" --output "$output" 
 
   rm -r results
+
+python main.py --input-video input_video/video_1.mp4 --output output_video/video_1.mp4
+python main.py --input-video input_video/video_3.mp4 --output output_video/video_3.mp4 # **
+
+python main.py --input-video input_video/video_4.mp4 --output output_video/video_4.mp4
+python main.py --input-video input_video/video_5.mp4 --output output_video/video_5.mp4
+python main.py --input-video input_video/video_6.mp4 --output output_video/video_6.mp4 # 保留
+python main.py --input-video input_video/video_7.mp4 --output output_video/video_7.mp4
+python main.py --input-video input_video/video_8.mp4 --output output_video/video_8.mp4
